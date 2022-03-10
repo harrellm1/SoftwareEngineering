589768024d2e0b918e6f2c24f2580a2faa47aaf7
harrellm1 <101367991+harrellm1@users.noreply.github.com>
2022-03-10 17:45:17 -0500

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+
+     public static void main (String [] args) {
+          int x = 1;
+          int y = 2;
+          int z = x + y;
+          System.out.println(z);
+     }
+}
